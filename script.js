4a3edf9ff5412eeda29b5da8782f4354ee31fe25
Ishika Verma <ishikaverma2317@gmail.com>
2025-09-27 09:34:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// Javascript
+// test_spec.js
+describe('Alphabetical Article Sorter', () => {
+  beforeEach(() => {
+    cy.visit('/main.html'); // Adjust path as needed
+  });
+
+  it('presence of the list - contains an unordered list with id bands', () => {
+    cy.get('#bands').should('be.visible');
+  });
+
+  it('should display articles in correct sorted order', () => {
+    cy.get('#bands li').should('have.length', 13);
+    
+    const expectedOrder = [
+      'Anywhere But Here',
+      'The Bled',
+      'Counterparts',
+      'The Devil Wears Prada',
+      'The Midway State',
+      'Norma Jean',
+      'Oh, Sleeper',
+      'An Old Dog',
+      'Pierce the Veil',
+      'The Plot in You',
+      'Say Anything',
+      'A Skylit Drive',
+      'We Came as Romans'
+    ];
+
+    expectedOrder.forEach((article, index) => {
+      cy.get('#bands li').eq(index).should('contain', article);
+    });
+  });
+});
